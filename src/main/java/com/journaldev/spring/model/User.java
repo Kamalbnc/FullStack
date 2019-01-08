300218f3d6861c6f7190170844f114b5286733f8
kamalbnc <kamalbnc@gmail.com>
2019-01-08 17:14:52 +0530

    commit

---
+++ src/main/java/com/journaldev/spring/model/User.java
@@ -0,0 +1,21 @@
+/*
+ * Developed by Kamal
+ * POJO Class for User Address
+ * 
+ */
+
+package com.journaldev.spring.model;
+
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		
+		return userName;
+		
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
