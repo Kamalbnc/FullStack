f328869e7d980e93e7821dea9a3c8ff9aa8a9a46
kamalbnc <kamalbnc@gmail.com>
2019-01-08 17:13:44 +0530

    commit

---
+++ src/main/java/com/journaldev/spring/model/Address.java
@@ -0,0 +1,43 @@
+/*
+ * Developed by Kamal
+ * POJO Class for User Address
+ * 
+ */
+
+
+package com.journaldev.spring.model;
+
+public class Address{
+	
+	public String city;
+	public String state;
+	public int pin;
+	
+	public String country;
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	
+	
+}
